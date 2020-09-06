33652a37f3bcfe41fcce60ef51b7b42927744345
zaiba19 <zaibaiqbal1@gmail.com>
2020-09-05 21:15:43 -0400

    Added basis of nonfunctioning delete

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -6,16 +6,17 @@ import { connect } from "react-redux";
     state = {
        todo: ""
     };
-  
+
     handleChange = (e) => this.setState({todo: e.target.value});
     handleSubmit = e => {
         e.preventDefault();
         this.props.addTodo(this.state.todo);
+        this.setState({todo:""});
     }
     render() {
         return (
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" placeholder="Enter your todo.." name="todo" onChange={this.handleChange}/>
+                    <input type="text" placeholder="Enter your todo.." value={this.state.todo} name="todo" onChange={this.handleChange}/>
                 </form>
         )
     }
