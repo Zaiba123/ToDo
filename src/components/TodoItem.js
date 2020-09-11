ae0e3f0e4b57ca60825a79fb7cc9146dfb55972a
zaiba19 <zaibaiqbal1@gmail.com>
2020-09-11 17:55:38 -0400

    adding more features like edit and delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,10 +8,35 @@ import { todosState } from '../recoil/atoms.js';
 import {atom } from  'recoil'
 import List from './List'
 
+function replaceItemAtIndex(arr,index,newValue){
+    return [...arr.splice(0,index),newValue,...arr.slice(index+1)];
+}
+function removeItemAtIndex(arr,index,newValue){
+    return [...arr.splice(0,index),...arr.slice(index+1)];
+}
 
 const TodoItem = (item) => {
-    // const [List, setList] = useRecoilValue(todosState);
-    // const index = List.findIndex((listItem) => listItem === item);
+    const [List, setList] = useRecoilValue(todosState);
+    const index = List.findIndex((listItem) => listItem === item);
+
+    const editItemText = ({target: {value}}) =>{
+        const newList = replaceItemAtIndex(List,index, {
+            ...item,
+            text:value,
+        });
+        setList(newList);
+    }
+    const toggleItemCompletion = () => {
+        const newList = replaceItemAtIndex(List,index,{
+            ...item,
+            isDone:!item.isDone,
+        });
+        setList(newList);
+    };
+    const deleteItem =() =>{
+        const newList = removeItemAtIndex(List,index);
+        setList(newList);
+    };
     // const handleChange = (e) => addInputTitle(e.target.value);
     // const handleSubmit = e => {
     //     e.preventDefault();
@@ -25,7 +50,14 @@ const TodoItem = (item) => {
     //     }
     // };
     return(
-        <div>hi</div>
+        <div>
+            <input type="text" value={item.text} onChange={editItemText} />
+            <input 
+            type="checkbox"
+            checked={item.isDone}
+            onChange={toggleItemCompletion}
+            />
+        </div>
     )
 }
 
