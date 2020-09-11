5ce04f38591ab1cb213d4fd18063f8f6554e54b3
zaiba19 <zaibaiqbal1@gmail.com>
2020-09-10 23:31:55 -0400

    starting to write addtodo

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import {addTodo, addInputTitle, editInputBox} from "../action/addTodo.action"
 import { Button } from '@material-ui/core';
-import { connect } from "react-redux";
+import {Todo} from './Todo.js';
 import AddIcon from '@material-ui/icons/Add';
 import "./style.css";
 
- export default class InputField extends React.Component {
-    handleChange = (e) => this.props.addInputTitle(e.target.value);
-    handleSubmit = e => {
-        e.preventDefault();
-        if(this.props.currentItem)
-        {
-            this.props.editInputBox({value:this.props.inputTitle,currentItem:this.props.currentItem})
-        }
-        else if ( this.props.inputTitle.length!==0)
-        {
-            this.props.addTodo(this.props.inputTitle);
-        }
-    };
-    render() {
-        return (
-                <form>
-                    <input type="text" placeholder="Enter a task.." value={this.props.inputTitle} name="todo" onChange={this.handleChange}/>
-                    <Button type="submit"  variant="outlined" value={this.props.inputTitle} name="todo" onClick={this.handleSubmit}> Add Item  <AddIcon/></Button>
-                </form>
-        )
-    }
+const InputField = () => {
+    // const handleChange = (e) => addInputTitle(e.target.value);
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     if(currentItem)
+    //     {
+    //         editInputBox({value:inputTitle,currentItem:currentItem})
+    //     }
+    //     else if ( inputTitle.length!==0)
+    //     {
+    //         addTodo(inputTitle);
+    //     }
+    // };
+    return(
+        <div>hi</div>
+    )
 }
-// const mapDispatchToProps = (dispatch) => ({
-//     addTodo : todo => dispatch(addTodo(todo)),
-//     addInputTitle: value => dispatch(addInputTitle(value)),
-//     editInputBox: obj => dispatch(editInputBox(obj))
-// });
-// const mapStateToProps = state => ({ //get value of text
-//     inputTitle: state.inputTitle,
-//     currentItem:state.currentItem
-// });
-// export default connect(
-//     mapStateToProps, //mapStateToProps should come before Dispatch
-//     mapDispatchToProps)(InputField);
+
+//  export default class InputField extends React.Component {
+//     handleChange = (e) => this.props.addInputTitle(e.target.value);
+//     handleSubmit = e => {
+//         e.preventDefault();
+//         if(this.props.currentItem)
+//         {
+//             this.props.editInputBox({value:this.props.inputTitle,currentItem:this.props.currentItem})
+//         }
+//         else if ( this.props.inputTitle.length!==0)
+//         {
+//             this.props.addTodo(this.props.inputTitle);
+//         }
+//     };
+//     render() {
+//         return (
+//                 <form>
+//                     <input type="text" placeholder="Enter a task.." value={this.props.inputTitle} name="todo" onChange={this.handleChange}/>
+//                     <Button type="submit"  variant="outlined" value={this.props.inputTitle} name="todo" onClick={this.handleSubmit}> Add Item  <AddIcon/></Button>
+//                 </form>
+//         )
+//     }
+// }
+export default InputField;
--- src/recoil/atoms.js
+++ src/recoil/atoms.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { atom,useRecoilState } from "recoil";
 
+export const todos = atom ({
+    key: 'items',
+    default: []
+})
+
+const darkModeState = atom ({
+    key:'darkMode',
+    default: true
+})
